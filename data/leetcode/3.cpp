36fd807d04f7727a4d4ed67c8d4d3b985829e6b8
zhuwenboa <https://zhuwenboa@github.com>
2020-02-18 11:50:48 +0800

    leetcode第三题

---
+++ data/leetcode/3.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<set>
+#include<map>
+using namespace std;
+
+class Solution 
+{
+public:
+    //单纯使用set，例如abccabd,就会导致i无意义的从a遍历到c。
+    /*
+    int lengthOfLongestSubstring(string s) 
+    {
+        int len = s.size();
+        set<char> judge;
+        int i = 0, j = 0;
+        int max_size = 0;
+        while(i < len && j < len)
+        {
+            if(judge.count(s[j]) == 0)
+            {
+                judge.insert(s[j]);
+                ++j;
+                max_size = max(max_size, j - i);
+            }
+            else
+            {
+                judge.erase(s[i]);
+                ++i;
+            }
+        }
+        return max_size;
+    }
+    */
+    /*
+    *使用map映射，一次越过无效的区间,这个是自己的思路写出的代码，因为要将之前的map中的元素均删除浪费时间，
+    *看了题解之后发现有更方便更快捷的方法，不用进行删除
+    */
+    /*
+    int lengthOfLongestSubstring(string s)
+    {
+        map<char, int> judge;
+        int len = s.size();
+        int i = 0, j = 0;
+        int max_size = 0;
+        int k = 0;
+        while(i < len && j < len)
+        {
+            if(judge.count(s[j]) == 0)
+            {
+                judge.insert({s[j], j});
+                ++j;
+                max_size = max(max_size, j - i);
+            }
+            else
+            {
+                int deadline = judge[s[j]];
+                k = i;
+                i = max(i, deadline);
+                for(; k <= i; ++k)
+                    judge.erase(s[k]);
+                judge.insert({s[j], j});
+                ++j;   
+                ++i;
+            }
+        }
+        return max_size;
+    }
+    */
+    int lengthOfLongestSubstring(string s)
+    {
+        map<char, int> judge;
+        int len = s.size();
+        int i = 0, j = 0;
+        int max_size = 0;
+        for(; j < len; ++j)
+        {
+            if(judge.count(s[j]))
+            {
+                i = max(i, judge[s[j]]);
+            }
+            max_size = max(max_size, j - i + 1);
+            judge[s[j]] = j + 1;
+        }
+        return max_size;
+    }
+};
+
+int main()
+{
+    string s;
+    cin >> s;
+    Solution res;
+    std::cout << res.lengthOfLongestSubstring(s) << "\n";
+}

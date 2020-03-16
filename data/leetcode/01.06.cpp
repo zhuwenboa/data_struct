7cf49c189b5f5abb245fb15b0d7d55f221fa1cb5
zhuwenboa <https://zhuwenboa@github.com>
2020-03-16 11:48:37 +0800

    01.06面试题

---
+++ data/leetcode/01.06.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+    string compressString(string S) {
+        if ((int)S.length() == 0) return S; // 空串处理
+        string ans = "";
+        int cnt = 1;
+        char ch = S[0];
+        for (int i = 1; i < (int)S.length(); ++i){
+            if (ch == S[i]) cnt++;
+            else{
+                ans += ch + to_string(cnt); // 注意 cnt 要转为字符串
+                ch = S[i];
+                cnt = 1;
+            }
+        }
+        ans += ch + to_string(cnt);
+        return ans.length() >= S.length() ? S : ans;
+    }
+};

4c232aca7926f29d3d138a3db22c141d8f76e94f
zhuwenboa <879658730@qq.com>
2020-05-02 22:37:12 +0800

    无重复的最长子串

--- data/leetcode/3.cpp
+++ data/leetcode/3.cpp
@@ -35,7 +35,7 @@ public:
     */
     /*
     *使用map映射，一次越过无效的区间,这个是自己的思路写出的代码，因为要将之前的map中的元素均删除浪费时间，
-    *看了题解之后发现有更方便更快捷的方法，不用进行删除
+    *看了题解之后发现有更方便更快捷的方法，不用进行删除,其实这个删除的思路和上面set好像没啥区别
     */
     /*
     int lengthOfLongestSubstring(string s)
@@ -68,6 +68,7 @@ public:
         return max_size;
     }
     */
+    //滑动窗口
     int lengthOfLongestSubstring(string s)
     {
         map<char, int> judge;
@@ -81,7 +82,7 @@ public:
                 i = max(i, judge[s[j]]);
             }
             max_size = max(max_size, j - i + 1);
-            judge[s[j]] = j + 1;
+            judge[s[j]] = j + 1;//用j+1，是为了每次找到重复字符的下一个位置
         }
         return max_size;
     }

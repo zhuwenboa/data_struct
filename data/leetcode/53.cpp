26851a1623879c5ebf09aa44de56158e72143f5e
zhuwenboa <879658730@qq.com>
2020-05-03 15:21:22 +0800

    最大子序和 动态规划法和分治法

--- data/leetcode/53.cpp
+++ data/leetcode/53.cpp
@@ -22,9 +22,42 @@ public:
     }
 };
 
+//分治法 线段树
+class Solution2
+{
+public:  
+    struct status 
+    {
+        //lsum表示[l,r]区间内以l为左端点的最大子段和,rsum表示[l,r]内以r为右端点的最大子段和
+        //msum表示[l,r]内的最大子段和，isum表示[l,r]的区间和
+        int lsum, rsum, msum, isum;
+    };
+    status pushup(status l, status r)
+    {
+        int isum = l.isum + r.isum;
+        int lsum = max(l.lsum, l.isum + r.lsum);
+        int rsum = max(r.rsum, l.rsum + r.isum);
+        int msum = max(max(l.msum, r.msum), l.rsum + r.lsum);
+        return status{lsum, rsum, msum, isum};
+    }
+    status get(vector<int>& vec, int l, int r)
+    {
+        if(l == r)
+            return status{vec[l], vec[l], vec[l], vec[l]};
+        int mid = (l + r) >> 1;
+        status lsub = get(vec, l, mid);
+        status rsub = get(vec, mid + 1, r);
+        return pushup(lsub, rsub);
+    }
+    int maxSubArray(vector<int>& nums)
+    {
+        return get(nums, 0, nums.size()-1).msum;
+    }
+};
+
 int main()
 {
-    Solution solut;
+    Solution2 solut;
     vector<int> a;
     int temp;
     while(cin >> temp)

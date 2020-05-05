8d6d968e2ade6bb75ffa54f440e50005781fad81
zhuwenboa <879658730@qq.com>
2020-05-05 22:03:33 +0800

    最接近的K个数 二分查找

--- data/leetcode/658.cpp
+++ data/leetcode/658.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
+#include<deque>
+//找到最接近的K个数 二分查找
 using namespace std;
 
 class Solution {
@@ -13,7 +14,6 @@ public:
         int len = arr.size();
         if(x > arr[len -1])
             return vector<int>(&arr[len - 1- k], &arr[len]);
-        
         int MinApproach = Binarysearch(arr, x);
         int i, j;
         i = MinApproach;
@@ -41,26 +41,25 @@ public:
         int high = arr.size() -1;
         int low = 0;
         int mid;
-        while(low <= high)
+        while(low+1 < high)
         {
             mid = (low + high) / 2;
-            if(x < arr[mid])
-            {
-                high = mid -1;
-            }
-            if(x > arr[mid])
-                low = mid + 1;
-            return mid;
+            if(x <= arr[mid])
+                high = mid;
+            else 
+                low = mid;
         }
-        return mid;
+        return low;
     }
 };
 
+
 int main()
 {
-    vector<int> arr{1, 1, 1, 10, 10, 10};
+    //vector<int> arr{0,1,2,3,4,4,4,5,5,5,6,7,9,9,10,10,11,11,12,13,14,14,15,17,19,19,22,24,24,25,25,27,27,29,30,32,32,33,33,35,36,38,39,41,42,43,44,44,46,47,48,49,52,53,53,54,54,57,57,58,59,59,59,60,60,60,61,61,62,64,66,68,68,70,72,72,74,74,74,75,76,76,77,77,80,80,82,83,85,86,87,87,92,93,94,96,96,97,98,99};
+    vector<int> arr{0,0,1,2,3,3,4,7,7,8};
     Solution res;
-    vector<int> vec = res.findClosestElements(arr, 1, 9);
+    vector<int> vec = res.findClosestElements(arr, 3, 5);
     for(auto a : vec)
         std::cout << a << "\n";
 }

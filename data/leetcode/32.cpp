f1d381be301e1434ea66a31f0af6f0d2f6e95cf2
zhuwenboa <879658730@qq.com>
2020-07-10 21:33:46 +0800

    最长有效括号 利用栈巧妙解决

---
+++ data/leetcode/32.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    int longestValidParentheses(string s) 
+    {
+        int maxans = 0;
+        stack<int> stk;
+        stk.push(-1);
+        for (int i = 0; i < s.length(); i++) 
+        {
+            if (s[i] == '(') 
+            {
+                stk.push(i);
+            } 
+            else 
+            {
+                stk.pop();
+                if (stk.empty()) 
+                {
+                    stk.push(i);
+                } 
+                else 
+                {
+                    maxans = max(maxans, i - stk.top());
+                }
+            }
+        }
+        return maxans;
+    }
+};

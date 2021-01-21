6ca3616080556cfcfaca999e5f8b28f2c7c29969
zhuwenboa <879658730@qq.com>
2021-01-21 10:55:15 +0800

    组合总和 回溯算法

--- data/leetcode/backtrack/39.cpp
+++ data/leetcode/backtrack/39.cpp
@@ -35,3 +35,34 @@ public:
         return ans;
     }
 };
+
+//回溯算法
+class Solution2 
+{
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+    {
+        if(candidates.empty())
+            return {};
+        vector<vector<int>> ans;
+        vector<int> cur;
+        backtrack(candidates, ans, cur, 0, 0, target);
+        return ans;
+    }
+    void backtrack(vector<int>& candidates, vector<vector<int>>& ans, vector<int>& cur, int sum, int start, const int& target)
+    {
+        if(sum == target)
+        {
+            ans.emplace_back(cur);
+            return;
+        }
+        for(int i = start; i < candidates.size(); ++i)
+        {
+            if(sum + candidates[i] > target)
+                continue;
+            cur.emplace_back(candidates[i]);
+            backtrack(candidates, ans, cur, sum + candidates[i], i, target);
+            cur.pop_back();
+        }
+    }
+};

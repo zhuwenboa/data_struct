f36891c9d6c96f0c6a0f6e3c0e08317f7b1aa094
zhuwenboa <879658730@qq.com>
2021-01-23 06:09:16 +0800

    二进制中1的个数 位运算

---
+++ data/leetcode/剑指offer/15.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    int hammingWeight(uint32_t n) 
+    {
+        int count = 0;
+        while(n)
+        {
+            count += 1;
+            n &= (n-1);
+        }
+        return count;
+    }
+};

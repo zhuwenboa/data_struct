af94223a5aca797eebb6ef74066533726f3143dd
zhuwenboa <879658730@qq.com>
2021-02-22 20:02:44 +0800

    数据流中的中位数 方法一 list做底层容器实现 O(N)插入和查找复杂度

--- data/leetcode/剑指offer/41.cpp
+++ data/leetcode/剑指offer/41.cpp
@@ -2,6 +2,7 @@
 #include<list>
 #include<vector>
 #include<algorithm>
+#include<queue>
 
 using namespace std;
 
@@ -61,13 +62,80 @@ private:
     int sum;
 };
 
+struct compare 
+{
+    bool operator() (const int& a, const int& b)
+    {
+        return a > b;
+    }
+};
+
+class MedianFinder2 
+{
+public:
+    /** initialize your data structure here. */
+    MedianFinder2() 
+    {
+        count_ = 0;
+    }
+    
+    void addNum(int num) 
+    {
+        ++count_;
+        if(count_ == 1)
+        {
+            max_heap_.push(num);
+            return;
+        }
+        if(count_ % 2 == 1)
+        {
+            if(num > min_heap_.top())
+            {
+                max_heap_.push(min_heap_.top());
+                min_heap_.pop();
+                min_heap_.push(num);
+            }
+            else 
+                max_heap_.push(num);
+        }
+        else 
+        {
+            if(num < max_heap_.top())
+            {
+                min_heap_.push(max_heap_.top());
+                max_heap_.pop();
+                max_heap_.push(num);
+            }            
+            else 
+                min_heap_.push(num);
+        }
+    }
+    
+    double findMedian() 
+    {
+        if(count_ % 2 == 1)
+            return static_cast<double>(max_heap_.top());
+        else 
+        {
+            double l_mid = max_heap_.top();
+            double r_mid = min_heap_.top();
+            return (l_mid + r_mid) / 2;
+        }
+    }
+private:  
+    std::priority_queue<int> max_heap_;
+    std::priority_queue<int, vector<int>, compare> min_heap_;
+    int count_;    
+};
+
 int main()
 {
-    MedianFinder res;
+    MedianFinder2 res;
     res.addNum(1);
     std::cout << res.findMedian() << "\n";
     res.addNum(2);
     res.addNum(3);
+    res.addNum(4);
     //std::cout << res.sum << "\n";
     std::cout << res.findMedian() << "\n";
 }
